6d47a9e5eee344ec4adb66c17ae9a5003ccd232e
moro <ricardoromero4444@gmail.com>
2022-05-07 20:49:20 -0300

    💡 : Add comment on local development

--- src/initGun.ts
+++ src/initGun.ts
@@ -1,17 +1,18 @@
-import type { GunSchema } from 'gun'
-import Gun from 'gun/gun'
-import 'gun/sea'
+import type { GunSchema } from 'gun';
+import Gun from 'gun/gun';
+import 'gun/sea';
 
 export type Todo = {
-	title: string
-	done: boolean
-}
+	title: string;
+	done: boolean;
+};
 
 interface AppState extends Record<string, GunSchema> {
-	todos: Record<string, Todo>
+	todos: Record<string, Todo>;
 }
 
 export const gun = Gun<AppState>([
+	// Run pnpm start:gun to start a local gun node
 	'localhost:8765/gun',
-	'https://gun-manhattan.herokuapp.com/gun',
-])
+	'https://gun-manhattan.herokuapp.com/gun'
+]);
