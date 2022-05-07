c823e68e83d70de27672c4e7814e091a0e2b6d23
moro <ricardoromero4444@gmail.com>
2022-05-07 00:22:13 -0300

    ✨ : Add base gun auth form

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,41 @@
 <script lang="ts">
-	export let name: string;
+	import Gun from 'gun/gun';
+	import 'gun/sea';
+
+	// Connect GUN to other peers in network.
+	let gun = Gun(['http://localhost:8765/gun', 'https://gun-manhattan.herokuapp.com/gun']);
+
+	// Handle user registration and login
+	let user = gun.user();
+
+	let username: string, password: string;
+
+	function signUp() {
+		user.create(username, password, () => {		});
+	}
+
+	function signIn() {
+		user.auth(username, password);
+	}
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<h1>Todo</h1>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+<hr />
+<form>
+	<input bind:value={username} type="text" placeholder="username" />
+	<input bind:value={password} type="password" placeholder="password" />
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+	<input type="submit" value="sign in" on:click|preventDefault={signIn} />
+	<input type="button" value="sign up" on:click={signUp} />
+</form>
+<hr />
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<ul />
+
+<hr />
+<form id="said">
+	<input id="say" />
+	<input id="speak" type="submit" value="speak" />
+</form>
+<hr />
