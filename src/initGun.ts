c87e9f4f6a8ecabe8bfdb74e2cb25a3690effccb
moro <ricardoromero4444@gmail.com>
2022-05-07 20:48:32 -0300

    ✨ : Connect gun to peers

--- src/initGun.ts
+++ src/initGun.ts
@@ -1,13 +1,17 @@
-import type { GunSchema } from 'gun';
-import Gun from 'gun/gun';
+import type { GunSchema } from 'gun'
+import Gun from 'gun/gun'
+import 'gun/sea'
 
 export type Todo = {
-	title: string;
-	done: boolean;
-};
-
-interface AppState extends Record<string, GunSchema> {
-	todos: Record<string, Todo>;
+	title: string
+	done: boolean
 }
 
-export const gun = new Gun<AppState>();
+interface AppState extends Record<string, GunSchema> {
+	todos: Record<string, Todo>
+}
+
+export const gun = Gun<AppState>([
+	'localhost:8765/gun',
+	'https://gun-manhattan.herokuapp.com/gun',
+])
