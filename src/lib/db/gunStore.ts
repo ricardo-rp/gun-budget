b4a071f22dabae418ac32570ba3aef263b13f63f
moro <ricardoromero4444@gmail.com>
2022-05-11 13:46:28 -0300

    🏷️ : Fix gunStore types

--- src/lib/db/gunStore.ts
+++ src/lib/db/gunStore.ts
@@ -16,51 +16,75 @@ const gun = Gun<AppState>([
   'https://gun-manhattan.herokuapp.com/gun',
 ])
 
-import type {
-  GunSchema,
-  GunValueSimple,
-  IGunChain,
-  IGunInstance,
-  LEXQuery,
-} from 'gun'
+import type { GunSchema, IGunChain, IGunInstance } from 'gun'
 import { writable } from 'svelte/store'
 
-function createMapStore<
-  AppState extends Record<string, GunSchema>,
-  TNode extends Exclude<GunSchema, GunValueSimple>,
-  Ref extends IGunChain<
-    TNode,
+function createGunStore<
+  RootSchema extends Record<string, GunSchema>, // Root schema
+  NodeKey extends keyof RootSchema & string = keyof RootSchema & string, // 'todos'
+  ParentNode extends RootSchema[NodeKey] = RootSchema[NodeKey], // Record<string, Todo>
+  Node extends ParentNode[keyof ParentNode] = ParentNode[keyof ParentNode] // { title: string, done: boolean }
+>(
+  ref: IGunChain<
+    Node,
     IGunChain<
-      Record<string, TNode>,
-      IGunInstance<AppState>,
-      IGunInstance<AppState>,
-      keyof AppState & string
+      Record<string, Node>,
+      IGunInstance<RootSchema>,
+      IGunInstance<RootSchema>,
+      NodeKey
     >,
-    IGunInstance<AppState>,
-    keyof TNode & string
+    IGunInstance<RootSchema>,
+    keyof ParentNode & string
   >
->(ref: Ref) {
-  const { update, subscribe } = writable<TNode>({} as TNode)
+) {
+  const { update, subscribe } = writable<ParentNode>({} as ParentNode)
 
-  ref.on(function (data, key) {
-    if (data !== null) {
-      update((store) => ({ ...store, [key]: data }))
-    } else {
-      update((store) => {
-        delete store[key]
-        return store
-      })
-    }
+  ref.on((gunData, key) => {
+    update((svelteStore) => {
+      if (gunData === null) return deleteGunKeyFromStore(key, svelteStore)
+
+      return updateGunKeyInStore(key, gunData, svelteStore)
+    })
   })
 
   return {
     subscribe,
-    update: <K extends keyof TNode & string>(
-      key: LEXQuery<K>,
-      value: TNode[K] & GunValueSimple
-    ) => ref.get(key).put(value as any),
+    update: <Key extends keyof Node & string = keyof Node & string>(
+      key: Key,
+      value: Node[Key]
+    ) => {
+      const put = ref.get(key).put
+      const assertedPut = put as (value: Node[Key]) => ReturnType<typeof put>
+      return assertedPut(value)
+    },
   }
 }
 
 const todosRef = gun.get('todos').map()
-export const todos = createMapStore(todosRef)
+export const todos = createGunStore(todosRef)
+
+todos.update('done', true)
+
+function isObject(value: unknown): value is Exclude<object, null> {
+  return typeof value === 'object' && value !== null
+}
+
+function updateGunKeyInStore<Node, ParentNode>(
+  key: keyof ParentNode,
+  gunData: Node,
+  svelteStore: ParentNode
+) {
+  if (isObject(svelteStore)) return { ...svelteStore, [key]: gunData }
+
+  return svelteStore
+}
+
+function deleteGunKeyFromStore<ParentNode>(
+  key: keyof ParentNode,
+  svelteStore: ParentNode
+) {
+  if (!isObject(svelteStore)) return null
+
+  delete svelteStore[key]
+  return svelteStore
+}
